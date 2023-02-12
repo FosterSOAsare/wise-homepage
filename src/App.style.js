d14d2d6f3f5316dd0780d2158bd69bdc6b27da69
fostersoasare <fostersoasare@gmail.com>
2023-02-12 19:09:31 +0000

    Styling refactor

--- src/App.style.js
+++ src/App.style.js
@@ -6,6 +6,9 @@ const style = {
 			background: "transparent",
 		},
 	},
+	hero__title: { fontSize: "42px", fontWeight: "bold", color: "#37517E" },
+	hero__desc: { color: "#5d7079", marginTop: "20px", display: "block", lineHeight: "25px" },
+	cta__button: { background: "#008FC9", marginTop: "20px", height: "45px", borderRadius: "2px", paddingInline: "28px", textTransform: "none", fontWeight: "bold" },
 };
 
 export default style;
