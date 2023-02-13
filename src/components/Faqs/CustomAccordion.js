ce44b075b611a75803a018237f35d5b0db27935a
fostersoasare <fostersoasare@gmail.com>
2023-02-13 11:16:30 +0000

    Faq accordion refactor

--- src/components/Faqs/CustomAccordion.js
+++ src/components/Faqs/CustomAccordion.js
@@ -1,16 +1,23 @@
 import React from "react";
-import { Typography, Accordion, AccordionSummary, AccordionDetails } from "@mui/material";
+import { Typography, AccordionSummary, AccordionDetails } from "@mui/material";
+import MuiAccordion from "@mui/material/Accordion";
+import { styled } from "@mui/material/styles";
 
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+const Accordion = styled((props) => <MuiAccordion disableGutters elevation={0} square {...props} />)(({ theme }) => ({
+	"&:before": {
+		display: "none",
+	},
+}));
 
 function CustomAccordion(props) {
 	let { question, answer } = props;
 	return (
-		<Accordion sx={{ backgroundColor: "transparent", border: "none", boxShadow: "none" }}>
-			<AccordionSummary expandIcon={<ExpandMoreIcon sx={{ color: "#00A1DB" }} />} aria-controls="panel1a-content" id="panel1a-header">
+		<Accordion sx={{ backgroundColor: "transparent", border: "none", outline: "transparent", boxShadow: "none", borderBottom: "20px" }}>
+			<AccordionSummary expandIcon={<ExpandMoreIcon sx={{ color: "#00A1DB" }} />} aria-controls="panel1a-content" id="panel1a-header" sx={{ paddingInline: "10px" }}>
 				<Typography sx={{ color: "white", fontWeight: "bold" }}>{question}</Typography>
 			</AccordionSummary>
-			<AccordionDetails sx={{ color: "white" }}>{answer}</AccordionDetails>
+			<AccordionDetails sx={{ color: "white", borderBottom: "1px solid grey", paddingBottom: "40px", paddingInline: "10px" }}>{answer}</AccordionDetails>
 		</Accordion>
 	);
 }
