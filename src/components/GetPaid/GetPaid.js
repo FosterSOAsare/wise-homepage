fe63f7a69aebcf7f5ae9ecbbebaaf428326cc774
fostersoasare <fostersoasare@gmail.com>
2023-02-22 22:14:58 +0000

    GetPaid responsiveness

--- src/components/GetPaid/GetPaid.js
+++ src/components/GetPaid/GetPaid.js
@@ -1,21 +1,19 @@
 import React from "react";
 import { Box, Container, Grid, Typography, Button } from "@mui/material";
 import GetPaidImage from "../../assets/images/get-paid.png";
-import { ReactComponent as Invoices } from "../../assets/images/invoices.svg";
-import { ReactComponent as CollectMoneySeamlessly } from "../../assets/images/collect-money.svg";
 import style from "../../App.style";
 
 const GetPaid = () => {
 	return (
 		<Box sx={style.getPaid}>
-			<Container maxWidth="lg" sx={{ padding: "100px 0" }}>
+			<Container maxWidth="lg" sx={{ paddingBlock: { xs: "40px", md: "80px" } }}>
 				<Grid container>
-					<Grid item xs={6}>
+					<Grid item xs={12} md={6} sx={style.getPaid__textContent}>
 						<Typography variant="h2" sx={{ fontSize: "26px", fontWeight: "bold", color: "#37517E" }}>
 							Get paid faster.
 						</Typography>
 
-						<Typography variant="p" sx={{ color: "#5d7079", marginTop: "20px", display: "block", lineHeight: "25px", width: "75%" }}>
+						<Typography variant="p" sx={style.getPaid__desc}>
 							With Wise Business, you can get local bank details for 10 different currencies. Give them to your customers for an easy, free way to get paid. 50% of payments are instant
 							or arrive in an hour.
 						</Typography>
@@ -27,18 +25,16 @@ const GetPaid = () => {
 							</Button>
 						</a>
 
-						<Box sx={{ marginTop: "25px", display: "flex", width: "80%", gap: "10px", justifyContent: "space-between" }}>
-							<Box sx={{ width: "48%" }}>
-								<CollectMoneySeamlessly />
-								<Typography variant="p" sx={{ fontWeight: "bold", display: "block", color: "#5d7079", fontSize: "15px", marginBottom: "10px" }}>
+						<Box sx={style.getPaid__pros}>
+							<Box sx={{ width: { xs: "100%", md: "48%" }, marginBottom: { xs: "30px", md: 0 } }}>
+								<Typography variant="p" sx={style.getPaid__pro__heading}>
 									Collect money seamlessly
 								</Typography>
-								<Typography variant="p" sx={{ lineHeight: "25px", fontWeight: "400", color: "#5d7079" }}>
+								<Typography variant="p" sx={{ lineHeight: "25px", fontWeight: "400", color: "#5d7079", width: "100%" }}>
 									Easily withdraw from Amazon, Stripe, and more in up to 10 currencies.
 								</Typography>
 							</Box>
-							<Box sx={{ width: "48%" }}>
-								<Invoices />
+							<Box sx={{ width: { xs: "100%", md: "48%" } }}>
 								<Typography variant="p" sx={{ fontWeight: "bold", display: "block", color: "#5d7079", fontSize: "15px", marginBottom: "10px" }}>
 									Create clever invoices
 								</Typography>
@@ -54,7 +50,7 @@ const GetPaid = () => {
 							</Box>
 						</Box>
 					</Grid>
-					<Grid item xs={6}>
+					<Grid item xs={12} md={6} sx={style.getPaid__image}>
 						<img src={GetPaidImage} alt="" />
 					</Grid>
 				</Grid>
--- src/components/GetPaid/GetPaid.style.js
+++ src/components/GetPaid/GetPaid.style.js
@@ -1,4 +1,10 @@
 const getPaidStyle = {
-	getPaid: { height: { md: "700px", sx: "auto" }, backgroundColor: "red" },
+	getPaid: { height: { md: "700px", sx: "auto" } },
+	getPaid__textContent: { order: { xs: "2", md: "1" }, marginTop: { xs: "60px", md: "0" } },
+	getPaid__image: { order: { xs: "1", md: "2" }, display: "flex", justifyContent: "center", alignItems: "center", margin: 0 },
+
+	getPaid__pros: { marginTop: "25px", display: "flex", width: { md: "80%", xs: "98%" }, gap: "10px", justifyContent: "space-between", flexDirection: { xs: "column", md: "row" } },
+	getPaid__desc: { color: "#5d7079", marginTop: "20px", display: "block", lineHeight: "25px", width: { xs: "98%", md: "75%" } },
+	getPaid__pro__heading: { fontWeight: "bold", display: "block", color: "#5d7079", fontSize: "15px", marginBottom: "10px" },
 };
 export default getPaidStyle;
